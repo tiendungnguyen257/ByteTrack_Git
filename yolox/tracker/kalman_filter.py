091600886c0cce5890b23f73ac138ad8f581f5fc
tiendungnguyen257 <30386144+tiendungnguyen257@users.noreply.github.com>
2024-09-19 14:07:24 +0700

    Update kalman_filter.py
    test conflict 2

--- yolox/tracker/kalman_filter.py
+++ yolox/tracker/kalman_filter.py
@@ -73,7 +73,7 @@ class KalmanFilter(object):
         mean_vel = np.zeros_like(mean_pos)
         mean = np.r_[mean_pos, mean_vel]
         
-        mean1 = mean // test conflict
+        mean1 = mean // test conflict, something wrong here
         
         std = [
             2 * self._std_weight_position * measurement[3],
